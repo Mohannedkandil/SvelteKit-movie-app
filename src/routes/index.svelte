1d5a3723d3833be2551004d5dcfea12248d181b2
Mohannedkandil <mohanadmkandil@gmail.com>
2022-01-23 02:15:53 +0200

    Style: Movies page in CSS Grid and FlexBox

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/popular?api_key=4145727dbbf8fd5c92455304d5710eaa&language=en-US&page=1`
+		);
+		const data = await res.json();
+		console.log(data);
+		if (res.ok) {
+			return {
+				props: { popular: data.results }
+			};
+		}
+	}
+</script>
+
+<script>
+	import PopularMovies from '../components/PopularMovies.svelte';
+	export let popular;
+</script>
+
+<section>
+	<PopularMovies {popular} />
+</section>
